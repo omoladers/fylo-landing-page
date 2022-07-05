f17d47c49a66aac0085d0155c1f0ebad8bce4be8
omoladers <omoladerasheeds@gmail.com>
2022-07-05 18:04:46 +0100

    fylo landing

---
+++ index.js
@@ -0,0 +1,57 @@
+const numbers = [0, 3.14, 9.81, 37, 98.6, 100]; // array of numbers
+const fruits = ["banana", "orange", "mango", "lemon"]; // array of strings, fruits
+const vegetables = ["Tomato", "Potato", "Cabbage", "Onion", "Carrot"]; // array of strings, vegetables
+const animalProducts = ["milk", "meat", "butter", "yoghurt"]; // array of strings, products
+const webTechs = ["HTML", "CSS", "JS", "React", "Redux", "Node", "MongDB"]; // array of web technologies
+const countries = ["Finland", "Denmark", "Sweden", "Norway", "Iceland"]; // array of strings, countries
+
+// Print the array and its length
+
+console.log("Numbers:", numbers);
+console.log("Number of numbers:", numbers[2]);
+
+console.log("Fruits:", fruits);
+console.log("Number of fruits:", fruits.length);
+
+console.log("Vegetables:", vegetables);
+console.log("Number of vegetables:", vegetables.length);
+
+console.log("Animal products:", animalProducts);
+console.log("Number of animal products:", animalProducts.length);
+
+console.log("Web technologies:", webTechs);
+console.log("Number of web technologies:", webTechs.length);
+
+console.log("Countries:", countries);
+console.log("Number of countries:", countries.length);
+
+const arr = Array(); // creates an an empty array
+console.log(arr);
+
+const eightEmptyValues = Array(8).fill("X"); // it creates eight empty values
+console.log(eightEmptyValues); // [empty x 8]
+const numberss = [1, 2, 3, 4, 5];
+console.log(numberss.slice()); // -> it copies all  item
+let a = 0
+if (a > 0) {
+  console.log(`${a} is a positive number`)
+} else if (a < 0) {
+  console.log(`${a} is a negative number`)
+} else if (a == 0) {
+  console.log(`${a} is zero`)
+} else {
+  console.log(`${a} is not a number`)
+}let num = prompt('Enter number');
+switch (true) {
+  case num > 0:
+    console.log('Number is positive');
+    break;
+  case num == 0:
+    console.log('Numbers is zero');
+    break;
+  case num < 0:
+    console.log('Number is negative');
+    break;
+  default:
+    console.log('Entered value was not a number');
+}
